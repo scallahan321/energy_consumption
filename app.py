7bc447598b5131174052ca7552290af6ec10aecf
Sean Callahan <sean@Seans-MacBook-Air.local>
2022-02-02 18:50:25 -0700

    added checklist

--- app.py
+++ app.py
@@ -15,16 +15,18 @@ db = os.environ.get('DB')
 db_user = os.environ.get('DB_USERNAME')
 db_password = os.environ.get("DB_PASSWORD")
 
+#load state data csv for state names, abbreviations, and area in sq miles
+state_data = pd.read_csv("state_data.csv")
+
+#resource names and abbreviations
+resource_abrevs = ['CL','NG','PA','SO','WY','BM','HY','NU']
+resource_names = ['Coal','Natural Gas','Petroleum','Solar','Wind','Biomass','Hydroelectricity','Nuclear']
+
 # To connect MySQL database
 engine = sqlalchemy.create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db}/energy")
 conn = engine.connect()
 
-query = sqlalchemy.text("SELECT * FROM facts LIMIT 20")
-df = pd.read_sql(query,conn)
-
 app = dash.Dash(__name__)
-
-fig = px.bar(df, x="year", y="consumption", color="resource_id", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -32,12 +34,36 @@ app.layout = html.Div(children=[
     html.Div(children='''
         Dash: A web application framework for your data.
     '''),
+    dcc.Dropdown(
+        id = 'state_dropdown',
+        options=[
+        {'label': i, 'value': i} for i in state_data['state']] ,
+        value='Alabama'
+    ),
+
+    dcc.Checklist(
+        id = 'resource_checklist',
+        options=[ {'label': i,'value':j} for i, j in zip(resource_names, resource_abrevs)],
+        value=['CL']
+    ),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='graph-1'
     )
 ])
+@app.callback(
+    Output(component_id='graph-1',component_property='figure'),
+    Input(component_id='state_dropdown',component_property='value'),
+    Input(component_id ='resource_checklist',component_property='value')
+)
+
+def update_state(selected_state,selected_resources): 
+    #checked = selected_resources
+    query = sqlalchemy.text(f'SELECT * FROM {selected_state}')
+    df = pd.read_sql(query,conn)
+    criterion = df['resource_id'].map(lambda x: x in selected_resources)
+    fig = px.line(df.loc[criterion], x="year", y="consumption", color="resource_id",line_group="resource_id")
+    return fig
 
 if __name__ == '__main__':
     
